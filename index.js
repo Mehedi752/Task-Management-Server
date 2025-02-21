7c4e209789843322160108bf6c62286f3d19ae48
Mehedi <mehedihasansagor301@gmail.com>
2025-02-21 09:39:46 +0600

    complete the project

--- index.js
+++ index.js
@@ -67,6 +67,41 @@ async function run () {
       res.send(task)
     })
 
+    app.delete('/tasks/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await taskCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.put('/tasks/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const updatedTask = req.body
+      const updateDoc = {
+        $set: {
+          title: updatedTask.title,
+          category: updatedTask.category,
+          description: updatedTask.description,
+        }
+      }
+      const result = await taskCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
+    app.put('/tasks/moved/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const category = req.body.category
+      const updateDoc = {
+        $set: {
+          category: category
+        }
+      }
+        const result = await taskCollection.updateOne(query, updateDoc)
+        res.send(result)
+    })
+
 
   } finally {
     // Ensures that the client will close when you finish/error
